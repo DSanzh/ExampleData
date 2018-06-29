4687dcd1ae3ab50e622e9213d81d5dd9d1300b9e
DSanzh <dauylov.sanzhar@gmail.com>
2018-06-29 15:13:19 +0600

    empty  project

---
+++ ExampleData/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ExampleData
+//
+//  Created by Sanzhar on 6/29/18.
+//  Copyright © 2018 Sanzhar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
